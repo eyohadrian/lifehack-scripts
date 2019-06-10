a24207f93f26652f026f30ed3edc48ebf03ddfdf
Adrian <adrian.asensio.bdn@gmail.com>
2019-06-10 02:00:39 +0200

    improved code

--- js/megadede-value-ratio-scrappe.js
+++ js/megadede-value-ratio-scrappe.js
@@ -1,28 +1,31 @@
-const loadBtn = document.querySelector(".onclick.load-more-icon");
-const load = () => loadBtn.click();
-const media = document.querySelector(".media-container-all");
+const LOAD_BUTTON_SELECTOR = ".onclick.load-more-icon";
+const CONTAINER_SELECTOR = ".media-container-all";
+const NAME_SELECTOR = ".media-title";
+const VALUE_SELECTOR = ".value";
+const MAX_LIST = 10;
 
+const loadBtn = document.querySelector(LOAD_BUTTON_SELECTOR);
+const media = document.querySelector(CONTAINER_SELECTOR);
 
 const observer = new MutationObserver((mutations, observer) => mutations.forEach(mutation => {
   const btn = Array.from(mutation.addedNodes)
     .filter(el => el.className)
     .find(el => el.className.includes("onclick"));
 
-  const result = Array.from(mutation.target.children)
+  const results = Array.from(mutation.target.children)
     .filter(el => !el.className.includes("onclick"))
-    .map(el => {
-      const value = Number(el.querySelector(".value").innerText);
-      const name = el.querySelector(".media-title").innerText;
-      return {name, value}
-    }).sort((a,b) => a.value > b.value ? -1 : 1).slice(0,10);
+    .map(el => ({
+      name: el.querySelector(NAME_SELECTOR).innerText,
+      value: Number(el.querySelector(VALUE_SELECTOR).innerText)
+    })).sort((a,b) => a.value > b.value ? -1 : 1).slice(0,MAX_LIST);
 
   if(btn) {
     btn.click();
   } else {
-    console.log(result);
+    console.log(results);
     observer.disconnect();
   }
 }));
 
-observer.observe(media, { attributes: true, childList: true, characterData: true });
-load();
+observer.observe(media, { childList: true });
+loadBtn.click();
